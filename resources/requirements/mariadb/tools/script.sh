c9d3a551e5997b6c5ecf2192d3a04f6d74e443ea
aaitbelh <aaitbelh@student.1337.ma>
2025-05-23 15:57:25 +0100

    🗑️Remove deprecated Docker and Nginx configurations; add Git and Make installation to playbook

--- resources/requirements/mariadb/tools/script.sh
+++
@@ -1,8 +0,0 @@
-service mysql start
-mysql -u root -e -p$DB_PASSWORD "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_PASSWORD'"
-mysql -u root -p$DB_PASSWORD << EOF
-create database if not exists $DB_NAME;
-create user if not exists '$DB_USER'@'%' identified by '$DB_PASSWORD';
-GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
-FLUSH PRIVILEGES;
-EOF
